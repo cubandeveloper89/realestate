d3aeb0eb09a7107f56b43c9e9a0dd5791aa1e477
Camilo Garcia <cr7habana@gmail.com>
2025-09-02 16:05:04 -0400

    more animations

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,26 +5,38 @@ import { ref, onMounted } from "vue";
 import { SplitText } from 'gsap/SplitText'
 
 const subtitleRef = ref<HTMLElement | null>(null);
+const imageRef = ref<HTMLElement | null>(null);
 
 const animateText = () => {
   gsap.registerPlugin(SplitText);
+
   let subtitleSplit = SplitText.create(subtitleRef.value, {
   type: "chars",
-});
-subtitleSplit.chars.forEach((char, index) => {
-  gsap.from(char, {
-    duration: 0.75,
-    y: gsap.utils.random(-150, 150),
-    x: gsap.utils.random(-300, 300),
-    rotate: gsap.utils.random(-360, 360),
-    scale: gsap.utils.random(0, 2),
-    ease: "back.out",
-    color: "#5F7CE0",
-    opacity: 0,
-    delay: index * 0.01,
   });
 
-})
+  let charsTl = gsap.timeline();
+  
+  subtitleSplit.chars.forEach((char, index) => {
+
+    gsap.from(char, {
+      duration: 0.75,
+      y: gsap.utils.random(-150, 150),
+      x: gsap.utils.random(-300, 300),
+      rotate: gsap.utils.random(-360, 360),
+      scale: gsap.utils.random(0, 2),
+      ease: "back.out",
+      color: "#5F7CE0",
+      opacity: 0,
+      delay: index * 0.01,
+    });
+  })
+  charsTl.from(imageRef.value, {
+    delay: 1,
+    duration: 0.75,
+    x: -150,
+    ease: "power.out",
+    opacity: 0,
+  });
 }
 
 onMounted(() => {
@@ -46,6 +58,7 @@ onMounted(() => {
       class="mr-24"
       src="https://www.shutterstock.com/image-vector/work-progress-coming-soon-icon-600nw-2492263749.jpg"
       alt="work in progress"
+      ref="imageRef"
     />
   </main>
 </template>
